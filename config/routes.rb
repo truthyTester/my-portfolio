473e31f3105e164059d237a18596d592eba7d873
Oleg Tomillo <oleg@Olegs-MacBook-Pro.local>
2018-12-19 12:32:05 -0800

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :my_portfolios
+  resources :my_portfolios, except: [:show]
+  get 'portfolio/:id', to:'my_portfolios#show', as: 'my_portfolio_show'
 
-  get 'about-me', to: 'pages#about'
+  get 'about-me', to:'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs
