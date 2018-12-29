d92136d17012f88c08c56b64564d6af040d2a329
Oleg Tomillo <oleg@Olegs-MacBook-Pro.local>
2018-12-28 20:39:22 -0800

    Implemented image defaults for portfolio items

--- app/models/my_portfolio.rb
+++ app/models/my_portfolio.rb
@@ -1,3 +1,10 @@
 class MyPortfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_mage, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_mage ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
